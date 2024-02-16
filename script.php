54738af9dcbf20a01416797ac514b8b32d50add3
Polvere243 <danielercoli@gmail.com>
2024-02-16 21:30:59 +0100

    first part of the exercise done

--- script.php
+++ script.php
@@ -1,3 +1,21 @@
 <?php
-$_GET['text', 'badword']
+$text = $_GET["text"];
+$badword = $_GET["badword"];
+?>
 
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Badwords</title>
+        <meta charset="UTF-8">
+        <meta name="author" content="Daniele Ercoli">
+        <meta name="description" content="esercizio">
+        <meta name="keyword" content="html, php">
+        <meta name="viewport" content="width=device-width, initial-scale=1.8">
+    </head>
+
+    <body>
+        <p><?=$text?></p>
+        <h2><?=$badword?></h2>
+    </body>
+</html>
